71589e953ab4faa37c9292e921f45c126b7170c8
lolpro11 <lolpro11lolx@gmail.com>
2023-12-11 23:08:23 -0800

    fixes

--- src/snap.rs
+++ src/snap.rs
@@ -36,7 +36,6 @@ impl GTFSGraph {
                     let direction: String = service[service.len() - 1..].to_string();
                     let statement = "INSERT INTO timetable(id, onestop_id, route, stop, service, direction, time) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *";
                     let rows = client.query_one(statement, &[&format!("{}-{}-{}-{}", self.onestop_id, route, stop, service_id), &self.onestop_id, &route, &stop, &service_id,  &direction, &json_value.unwrap()]).await;
-                    println!("inserted");
                 }
             }
         }
@@ -138,7 +137,7 @@ async fn main() {
     let (client, connection) = tokio_postgres::connect(conn_string, NoTls).await.unwrap();
     tokio::spawn(connection);
     let x = client.execute(
-        "CREATE TABLE IF NOT EXISTS timetable (
+        "CREATE TABLE timetable (
             id VARCHAR PRIMARY KEY,
             onestop_id VARCHAR,
             time VARCHAR,
