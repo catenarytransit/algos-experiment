30ac7df72bc0c1c06c2069d153eaeada13f7734f
lolpro11 <lolpro11lolx@gmail.com>
2023-12-18 03:13:24 -0800

    Add points

--- src/geodesic.rs
+++ src/geodesic.rs
@@ -1,3 +1,5 @@
+use algo::{vincenty_inverse, Ellipsoid};
+
 mod lib; 
 fn dd_to_dms(degs: f64) -> (bool, u32, u32, f64) {
     let is_negative = degs < 0.0;
@@ -16,6 +18,9 @@ fn dms_str(val: f64) -> String {
     format!("{}{}° {}' {:.4}\"", if dms.0 { "-" } else { "" }, dms.1, dms.2, dms.3)
 }
 fn point_to_geodesic(pA: (f64, f64), pB: (f64, f64), pP: (f64, f64)) -> (f64, f64) {
+    let earth = Ellipsoid::from_descriptor(&algo::WGS84_ELLIPSOID_DESCRIPTOR);
+    let a_p = vincenty_inverse(pA.0, pA.1, pP.0, pP.1, &earth, 32767);
+    let a_b = vincenty_inverse(pA.0, pA.1, pB.0, pB.1, &earth, 32767);
     (0.0, 0.0)
 }
 fn main() {
