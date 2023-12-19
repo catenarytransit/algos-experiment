a4f917f4cef08b6e555d3106f503b172553d396c
lolpro11 <lolpro11lolx@gmail.com>
2023-12-18 16:46:12 -0800

    Add new graph structure

--- src/test.rs
+++ src/test.rs
@@ -8,110 +8,135 @@ const MAX: f64 = f64::MAX;
 
 #[derive(Debug, Clone)]
 struct Graph {
-    nodes: HashMap<String, HashMap<String, f64>>,
+    nodes: Vec<Node>,
+    edges: Vec<Edge>,
+}
+
+#[derive(Debug, Clone)]
+struct Node {
+    id: u64,
+    lon: f64,
+    lat: f64,
+}
+
+impl Node {
+    fn new(id: u64, lon: f64, lat: f64) -> Self {
+        Self {
+            id: id,
+            lon: lon,
+            lat: lat,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Edge {
+    id: String,
+    osm_id: String,
+    source: String,
+    target: String,
+    length: f64,
+    foot: bool,
+    car_forward: String,
+    car_backward: String,
+    bike_forward: bool,
+    bike_backward: bool,
+    train: String
+    //linestring: String,
+}
+
+impl Edge {
+    fn new(id: String, osm_id: String, source: String, target: String, length: f64, foot: bool, car_forward: String, car_backward: String, bike_forward: bool, bike_backward: bool, train: String) -> Self {
+        Self {
+            id: id,
+            osm_id: osm_id,
+            source: source,
+            target: target,
+            length: length,
+            foot: foot,
+            car_forward: car_forward,
+            car_backward: car_backward,
+            bike_forward: bike_forward,
+            bike_backward: bike_backward,
+            train: train,
+        }
+    }
 }
 
 impl Graph {
     fn new() -> Self {
         Self {
-            nodes: HashMap::new(),
+            nodes: Vec::new(),
+            edges: Vec::new(),
         }
     }
 
-    fn add_node(&mut self, node: &str, connections: HashMap<String, f64>) {
-        self.nodes.insert(node.to_string(), connections);
+    fn add_node(&mut self, id: u64, lon: f64, lat: f64) {
+        self.nodes.push(Node::new(id, lon, lat));
     }
 
-    fn add_edge(&mut self, source: String, target: String, weight: f64) {
-        self.nodes.get_mut(&source).unwrap().insert(target, weight);
+    fn add_node_obj(&mut self, node: Node) {
+        self.nodes.push(node);
+    }
+    fn add_edge(&mut self, id: String, osm_id: String, source: String, target: String, length: f64, foot: bool, car_forward: String, car_backward: String, bike_forward: bool, bike_backward: bool, train: String) {
+        self.edges.push(Edge::new(id, osm_id, source, target, length, foot, car_forward, car_backward, bike_forward, bike_backward, train))
     }
 
-    fn dijkstra(&self, start_node: &str, target_node: &str) -> Option<(f64, Vec<String>)> {
-        let mut dists: HashMap<String, f64> = self.nodes.keys().map(|node| (node.clone(), MAX)).collect();
-        let mut path: HashMap<String, String> = HashMap::new();
-        let mut min_heap = BinaryHeap::new();
-
-        dists.insert(start_node.to_string(), 0.0);
-        min_heap.push(DijkstraNode { node: start_node.to_string(), dist: 0.0 });
-
-        while let Some(DijkstraNode { node, dist }) = min_heap.pop() {
-            if &node == target_node {
-                break;
-            }
-
-            if let Some(neighbors) = self.nodes.get(&node) {
-                for (adj_node, weight) in neighbors.iter() {
-                    let new_dist = dist + weight;
-                    if new_dist < *dists.get(adj_node).unwrap_or(&MAX) {
-                        dists.insert(adj_node.clone(), new_dist);
-                        path.insert(adj_node.clone(), node.clone());
-                        min_heap.push(DijkstraNode { node: adj_node.clone(), dist: new_dist });
-                    }
-                }
-            }
-        }
-
-        if *dists.get(target_node).unwrap_or(&MAX) == MAX {
-            return None;
-        }
-
-        let mut final_path = vec![target_node.to_string()];
-        let mut current_node = target_node.to_string();
-        while let Some(prev_node) = path.get(&current_node) {
-            final_path.push(prev_node.clone());
-            current_node = prev_node.clone();
-        }
-
-        final_path.reverse();
-
-        Some((*dists.get(target_node).unwrap(), final_path))
+    fn add_edge_obj(&mut self, edge: Edge) {
+        self.edges.push(edge);
     }
-}
 
-#[derive(Debug, Clone, PartialEq, PartialOrd)]
-struct DijkstraNode {
-    node: String,
-    dist: f64,
-}
 
-impl Eq for DijkstraNode {}
-
-impl Ord for DijkstraNode {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.dist.partial_cmp(&other.dist).unwrap_or(Ordering::Equal)
-    }
 }
 
 fn main() {
     let mut graph = Graph::new();
-    let file = File::open("edges.csv").unwrap();
-
+    let edges = File::open("edges.csv").unwrap();
     let mut rdr = ReaderBuilder::new()
         .has_headers(true)
-        .from_reader(file);
+        .from_reader(edges);
 
     // Iterate over the CSV records
-    for result in rdr.records() {
-        // Extract the record
-        let record = result.unwrap();
-        //let id: String = record[0].to_string();
-        let source: String = record[2].to_string();
-        let target: String = record[3].to_string();
-        let length: f64 = record[4].parse().unwrap();
-        if graph.nodes.contains_key(&source) {
-            graph.add_edge(source, target, length);
-            continue;
-        }
-        let mut node = HashMap::new();
-        node.insert(target, length);
-        graph.add_node(source.as_str(), node);
+    for record in rdr.records() {
+        let record = record.unwrap();
+        let edge = Edge {
+            id: record[0].to_string(),
+            osm_id: record[1].parse().unwrap(),
+            source: record[2].parse().unwrap(),
+            target: record[3].parse().unwrap(),
+            length: record[4].parse().unwrap(),
+            foot: if record[5].parse::<String>().unwrap() == "Allowed" {
+                true
+            } else {
+                false
+            },            
+            car_forward: record[6].to_string(),
+            car_backward: record[7].to_string(),
+            bike_forward: if record[8].parse::<String>().unwrap() == "Allowed" {
+                true
+            } else {
+                false
+            },  
+            bike_backward: if record[9].parse::<String>().unwrap() == "Allowed" {
+                true
+            } else {
+                false
+            },
+            train: record[10].to_string(),
+        };
+        graph.add_edge_obj(edge);
     }
-
-    //println!("{:#?}", graph);
-    if let Some((distance, path)) = graph.dijkstra("297523835", "364042999") {
-        println!("Shortest Distance: {}", distance);
-        println!("Shortest Path: {:?}", path);
-    } else {
-        println!("No path found");
+    let nodes = File::open("nodes.csv").unwrap();
+    let mut rdr = ReaderBuilder::new()
+        .has_headers(true)
+        .from_reader(nodes);
+    for record in rdr.records() {
+        let record = record.unwrap();
+        let node = Node {
+            id: record[0].parse().unwrap(),
+            lon: record[1].parse().unwrap(),
+            lat: record[2].parse().unwrap(),
+        };
+        graph.add_node_obj(node);
     }
 }
