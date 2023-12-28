9bdc1da6e9b70e3f2bd8f1de7925bfabdd92844a
lolpro11 <lolpro11lolx@gmail.com>
2023-12-28 01:52:55 -0800

    add threads cmd args

--- src/test.rs
+++ src/test.rs
@@ -610,12 +610,15 @@ impl Graph {
 }
 
 fn main() {
+    let threads = arguments::parse(std::env::args())
+        .expect("Add --feeds <string>")
+        .get::<u32>("threads").unwrap_or_else(|| 16.to_owned());
     let start_time = Instant::now();
     let graph = Graph::from_csv("edges.csv", "nodes.csv");
     eprintln!("from_csv took {:?}", start_time.elapsed().as_secs_f64());
     let start_time = Instant::now();
-    let graph = Graph::from_csv_par3("edges.csv", "nodes.csv", 16);
+    let graph = Graph::from_csv_par3("edges.csv", "nodes.csv", threads);
     eprintln!("from_csv_par3 took {:?}", start_time.elapsed().as_secs_f64());
-    let graph = Graph::from_csv_par4("edges.csv", "nodes.csv", 16);
+    let graph = Graph::from_csv_par4("edges.csv", "nodes.csv", threads);
     eprintln!("from_csv_par4 took {:?}", start_time.elapsed().as_secs_f64());
 }
