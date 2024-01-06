7aced2f1f983673259a03db572726546cbd2c6c6
lolpro11 <lolpro11lolx@gmail.com>
2024-01-06 15:19:30 -0800

    ms too big, change to ns

--- src/nn.rs
+++ src/nn.rs
@@ -9,7 +9,6 @@ fn main() {
     //let graph = Graph::from_csv_par3("edges.csv", "nodes.csv", 32);
     let graph = Graph::from_csv("testedges.csv", "testnodes.csv");
     eprintln!("from_csv_par3 took {:?}", start_time.elapsed().as_secs_f64());
-    println!("Graph: {:#?}", graph);
     let tree = vpsearch::Tree::new(&graph.nodes);
     let (index, _) = tree.find_nearest(&Node {
         id: 2729462058,
@@ -17,5 +16,5 @@ fn main() {
         lat: 33.4837658,
     });
     let start_time = Instant::now();
-    println!("The nearest point, {}, is at ({}, {})\n Took {:?}", graph.nodes[index].id, graph.nodes[index].lat, graph.nodes[index].lon, start_time.elapsed().as_millis());
+    println!("The nearest point, {}, is at ({}, {})\n Took {:?}ns", graph.nodes[index].id, graph.nodes[index].lat, graph.nodes[index].lon, start_time.elapsed().as_nanos());
 }
